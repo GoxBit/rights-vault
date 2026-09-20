8e6fe4b68a557860b85135507c370fcc644da7f4
Luis Duran <61667213+GoxBit@users.noreply.github.com>
2026-09-20 14:52:53 -0600

    Revert "test: intentional failure to validate pipeline red state"
    This reverts commit c290bae55326f2d7cc12fc4929f0dd09cb3f4d95.

--- tests/RightsVault.Domain.Tests/LicenseAgreementTests.cs
+++ tests/RightsVault.Domain.Tests/LicenseAgreementTests.cs
@@ -219,13 +219,5 @@ public class LicenseAgreementTests
 
             Assert.IsAssignableFrom<DomainException>(exception);
         }
-
-        [Fact]
-        public void PipelineDetectaFallos_Rojo()
-        {
-            Assert.True(false, "Fallo intencional para validar el pipeline");
-        }
     }
-
-
 }
